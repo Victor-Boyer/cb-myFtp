86e80fa11c4d4ab04bfa4a63d1403a32f9326a6d
Victor <victorboyer33@gmail.com>
2020-11-22 20:28:03 +0100

    master push

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
-const { read } = require('fs');
-var net = require('net');
+const fs = require('fs');
+const net = require('net');
 const readLine = require('readline');
+const path = require('path');
 
 const rl = readLine.createInterface({
   input: process.stdin,
@@ -19,7 +20,7 @@ if (!id) {
     process.exit(0)
 }
 
-var client = new net.Socket();
+let client = new net.Socket();
 client.connect(port, id, function() {
   client.on('ready', function(){
     console.log('Hello, welcome to the TCP client/server.');
@@ -29,14 +30,35 @@ client.connect(port, id, function() {
 
 rl.on('line', (line) => {
   client.write(`${line}`);
+  if (line.substr(0, 4) == 'stor') {
+    console.log('executing...');  
+    let array_data = line.toString().split(' ');
+
+    try {
+      let file = fs.readFileSync(''+ array_data[1] + '');
+      let fileName = path.basename(''+ array_data[1] + '');
+
+      client.write( 'stor pipe_on |'+ file + '|' + fileName);
+    } catch (err) {
+      console.error('No such file called : ' + array_data[1]);
+    }
+  }
 });
 
 client.on('data', function(data) {
-  var msg =  data.toString();
-	console.log('Received: ' + msg);
-	//client.destroy(); // kill client after server's response
+  if(data.toString().substr(0, 4) == 'retr') {
+    let stor_data = data.toString().split('|');
+      try {
+        fs.writeFile('./ '+ stor_data[2].toString(), stor_data[1], function() {
+          console.log('Download done');
+        });
+      } catch (err) {
+          console.error('Error, cannot download the file. Maybe it already exist.');
+        }
+  } else console.log('Received: ' + data.toString());
 });
 
 client.on('close', function() {
-	console.log('Connection closed');
+  console.log('Connection closed');
+  process.exit(0)
 });
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ fs.stat('./save.json', function(err) {
   }
   else if (err.code === 'ENOENT') {
       console.log('Save file does not exist... Creating a new one...');
-      fs.writeFile('s ave.json', '',function (err) {
+      fs.writeFile('save.json', '',function (err) {
         if (err) throw err;
         console.log('Created !');
       }); 
@@ -25,17 +25,20 @@ if (!port) {
 
 const server = net.createServer((socket) => {
   socket.write('Hello client ! Glad to see you\n');
-  var cnt;
-  var userNameOk = false;
-  var passOk = false;
+  let cnt;
+  let userNameOk = false;
+  let passOk = false;
+  let isConnected = false;
 
   socket.on('data', function(data) {
-   var array_data = data.toString().split(' ');
+
+  let array_data = data.toString().split(' ');
 
     /* USER COMMAND */
     if(array_data[0] == 'USER' || array_data[0] == 'user') {
       for(let i=0; i < save.users.length; i++) {
         if(save.users[i].username == array_data[1]) {
+          cnt = i;
           userNameOk = true;
           socket.write('Username found : ' + save.users[i].username);
         }
@@ -49,66 +52,133 @@ const server = net.createServer((socket) => {
 
     /* PASS COMMAND */
     else if (array_data[0] == 'PASS' || array_data[0] == 'pass') {
-      for(let i=0; i < save.users.length; i++) {
-        if(save.users[i].password == array_data[1]) {
-          cnt = i;
-          socket.write('User found with this password: ' + save.users[cnt].username);
+      if(save.users[cnt].password == array_data[1]) {
+          socket.write('Good password ! Welcome back ' + save.users[cnt].username);
+          console.log('User '+ save.users[cnt].username + ' connected.');
           passOk = true;
-        }
+          isConnected = true;
       }
-      if (passOk == false) {
-        socket.write('0 user found. This password doesn\'t exist');
+      else if (passOk == false) {
+        socket.write('Wrong password for : ' + save.users[cnt].username);
       }
-      console.log('PASS command had been used.');
       passOk = false;
     }
     
     /* LIST COMMAND */
     else if (array_data[0] == 'LIST' || array_data[0] == 'list') {
-      fs.readdir(process.cwd(), (err, files) => {
-        socket.write('List of file from ' + process.cwd() + ' : ' )
-        files.forEach(file => {
-          socket.write(file);
-        });
-      });
-      socket.write('Current directory : ' + process.cwd());
+      if(isConnected == false) {
+        socket.write('You\'re not connected, use USER and PASS command to connect');
+      }
+      else {
+        try {
+          fs.readdir(process.cwd(), (err, files) => {
+            socket.write('List of file from ' + process.cwd() + ' : \n' )
+            files.forEach(file => {
+              socket.write(file + '\n');
+            });
+          });
+        } catch (err) {
+          socket.write('Error: cannot display the file list.')
+        }
+      }
     }
 
     /* CWD COMMAND */
     else if (array_data[0] == 'CWD' || array_data[0] == 'cwd') {
-      try {
-        process.chdir(array_data[1]);
-        socket.write(`New directory: ${process.cwd()}`);
-      } catch (err) {
-        socket.write('ERROR : path doesn\'t exist' + err)
-        console.error(`ERROR : chdir: ${err}`);
+      if(isConnected == false) {
+        socket.write('You\'re not connected, use USER and PASS command to connect');
       }
+      else {
+        try {
+          process.chdir(array_data[1]);
+          socket.write(`New directory: ${process.cwd()}`);
+        } catch (err) {
+          socket.write('ERROR : path doesn\'t exist' + err)
+          console.error(`ERROR : chdir: ${err}`);
+        }
+      } 
     }
 
     /* PWD COMMAND */
     else if (array_data[0] == 'PWD' || array_data[0] == 'pwd') {
-      socket.write('Current directory : ' + process.cwd());
+      if(isConnected == false) {
+        socket.write('You\'re not connected, use USER and PASS command to connect');
+      }
+      else {
+        socket.write('Current directory : ' + process.cwd());
+      }
     }
 
     /* HELP COMMAND */
     else if (array_data[0] == 'HELP' || array_data[0] == 'help') {
-      socket.write('Here the list of all the commands you can use : ' + '\n' +
-                  'USER <username>: check if the user exist' + '\n' +
-                  'PASS <password>: authenticate the user with a password' + '\n' +
-                  'LIST: list the current directory of the server' + '\n' +
-                  'CWD <directory>: change the current directory of the server' + '\n' +
-                  'RETR <filename>: transfer a copy of the file FILE from the server to the client' + '\n' +
-                  'STOR <filename>: transfer a copy of the file FILE from the client to the server' + '\n' +
-                  'PWD: display the name of the current directory of the server' + '\n' +
-                  'HELP: send helpful information to the client' + '\n' +
-                  'QUIT: close the connection and stop the program'
-                  );
+      if(isConnected == false) {
+        socket.write('You\'re not connected, use USER and PASS command to connect');
+      }
+      else {
+        socket.write('Here the list of all the commands you can use : ' + '\n' +
+        'USER <username>: check if the user exist' + '\n' +
+        'PASS <password>: authenticate the user with a password' + '\n' +
+        'LIST: list the current directory of the server' + '\n' +
+        'CWD <directory>: change the current directory of the server' + '\n' +
+        'RETR <filename>: transfer a copy of the file FILE from the server to the client' + '\n' +
+        'STOR <filename>: transfer a copy of the file FILE from the client to the server' + '\n' +
+        'PWD: display the name of the current directory of the server' + '\n' +
+        'HELP: send helpful information to the client' + '\n' +
+        'QUIT: close the connection and stop the program'
+        );
+      }
+    }
+
+     /* STOR COMMAND */
+     else if (array_data[0] == 'STOR' || array_data[0] == 'stor') {
+      if(isConnected == false) {
+        socket.write('You\'re not connected, use USER and PASS command to connect');
+      }
+      else {
+        socket.write('stor ' + array_data[1]);
+        if(array_data[1] == 'pipe_on') {
+          let stor_data = data.toString().split('|');
+          try {
+            fs.writeFile('./storage/ '+ stor_data[2].toString(), stor_data[1], function() {
+              socket.write('Transfer done.');
+              console.log('Transfer done');
+            });
+          } catch (err) {
+            console.error('Error, cannot download the file. Maybe it already exist.');
+          }
+        }
+        console.log('STOR command had been used');
+      }
+    }
+
+    /* RETR COMMAND */
+     else if (array_data[0] == 'RETR' || array_data[0] == 'retr') {
+      if(isConnected == false) {
+        socket.write('You\'re not connected, use USER and PASS command to connect');
+      }
+      else {
+        console.log('executing...');
+        try {
+          let file = fs.readFileSync( process.cwd() +'/storage/'+ array_data[1] + '');
+          let fileName = path.basename('./storage/'+ array_data[1] + '');
+          socket.write( 'retr |'+ file + '|' + fileName);
+        } catch (err) {
+          socket.write('No such file called : ' + array_data[1]);
+        }
+      }
+      console.log('RETR command had been used');
+    }
+
+    /* QUIT COMMAND */
+    else if (array_data[0] == 'QUIT' || array_data[0] == 'quit') {
+      socket.write('You have been disconnected. See you soon !');
+      isConnected = false;
+      socket.emit('close')
     }
 
     else {
       socket.write('Error : command "' + array_data[0] + '" not found...')
     }
-
   });
 }).on('error', (err) => {
     // Handle errors here.
